33e6fcac2b9194a50cbef24506064c2e9661801c
Kadir Yilmaz <128429613+Kadirylmz1@users.noreply.github.com>
2023-06-08 00:30:51 +0300

    Add files via upload

---
+++ lib/homepage.dart
@@ -0,0 +1,54 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GNav(
+            backgroundColor: Colors.black,
+            color: Colors.white,
+            activeColor: Colors.white,
+            tabBackgroundColor: Colors.grey.shade800,
+            gap: 8,
+            onTabChange: (index) {
+              print(index);
+            },
+            padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 20),
+            tabs: const [
+              GButton(
+                icon: Icons.home,
+                text: "Home",
+              ),
+              GButton(
+                icon: Icons.favorite_border,
+                text: "Likes",
+              ),
+              GButton(
+                icon: Icons.search,
+                text: "Serach",
+              ),
+              GButton(
+                icon: Icons.settings,
+                text: "Settings",
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
